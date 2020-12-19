8603cffd668023fb4a9d065a72658448658d1e72
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-19 12:04:16 +0900

    ゴングのアルゴリズムを設定

--- MinuteRepeaterTest/ViewController.swift
+++ MinuteRepeaterTest/ViewController.swift
@@ -11,19 +11,43 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+
+  @IBAction func actionButton(_ sender: UIButton) {
     
     let now = Date()
     let time = Calendar.current.dateComponents([.hour, .minute], from: now)
 
-    let hour = time.hour!
+    var hour = time.hour!
+
+    if hour > 12 {
+      hour -= 12
+    }
+    
     let quarter = time.minute! / 15
     let minute = time.minute! % 15
-
-    print(hour)
-    print(quarter)
-    print(minute)
+    
+    var hourCount = 0
+    var quarterCount = 0
+    var minuteCount = 0
+    
+    // 第一ゴング
+    while hourCount < hour {
+      print(hourCount, "ding")
+      hourCount += 1
+    }
+    
+    // 第二ゴング
+    while quarterCount < quarter {
+      print(quarterCount, "ding-dong")
+      quarterCount += 1
+    }
+    
+    // 第二ゴング
+    while minuteCount < minute {
+      print(minuteCount, "dong")
+      minuteCount += 1
+    }
   }
-
-
 }
 
