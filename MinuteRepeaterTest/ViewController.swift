611557f13492fafd4010556e4195d9aa441fbfac
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-19 11:15:00 +0900

    値の取得と定義

--- MinuteRepeaterTest/ViewController.swift
+++ MinuteRepeaterTest/ViewController.swift
@@ -12,7 +12,16 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    let now = Date()
+    let time = Calendar.current.dateComponents([.hour, .minute], from: now)
 
+    let hour = time.hour!
+    let quarter = time.minute! / 15
+    let minute = time.minute! % 15
+
+    print(hour)
+    print(quarter)
+    print(minute)
   }
 
 
