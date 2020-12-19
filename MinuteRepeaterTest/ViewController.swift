c45be918a6188e0a3f8e5be9120912ff7540b8df
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-19 16:00:01 +0900

    遅延処理の実装

--- MinuteRepeaterTest/ViewController.swift
+++ MinuteRepeaterTest/ViewController.swift
@@ -6,18 +6,26 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
+//  var soundIdRing:SystemSoundID = 1016
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let soundIdRing:SystemSoundID = 1334
+    AudioServicesPlaySystemSound(soundIdRing)
   }
 
   @IBAction func actionButton(_ sender: UIButton) {
     
+    
     let now = Date()
     let time = Calendar.current.dateComponents([.hour, .minute], from: now)
-
+    
     var hour = time.hour!
 
     if hour > 12 {
@@ -31,23 +39,44 @@ class ViewController: UIViewController {
     var quarterCount = 0
     var minuteCount = 0
     
+    let lowGong:SystemSoundID = 1013
+    let highGong:SystemSoundID = 1005
+
     // 第一ゴング
     while hourCount < hour {
-      print(hourCount, "ding")
+      print(hourCount, "dong")
+      
       hourCount += 1
+      
+      AudioServicesPlaySystemSound(lowGong)
+      
+      // 0.7秒待つ
+      Thread.sleep(forTimeInterval: 0.7)
     }
     
     // 第二ゴング
     while quarterCount < quarter {
       print(quarterCount, "ding-dong")
       quarterCount += 1
-    }
+      
+      AudioServicesPlaySystemSound(highGong)
+      
+      // 0.7秒待つ
+      Thread.sleep(forTimeInterval: 0.5)
+      
+      AudioServicesPlaySystemSound(lowGong)
+      
+      // 0.7秒待つ
+      Thread.sleep(forTimeInterval: 0.7)    }
     
-    // 第二ゴング
+    // 第三ゴング
     while minuteCount < minute {
-      print(minuteCount, "dong")
+      print(minuteCount, "ding")
       minuteCount += 1
-    }
+      
+      AudioServicesPlaySystemSound(highGong)
+      // 0.7秒待つ
+      Thread.sleep(forTimeInterval: 0.7)    }
   }
 }
 
