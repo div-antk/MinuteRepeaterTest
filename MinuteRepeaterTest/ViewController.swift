2092ddd492d514ef0edb047517c0403f5d4c6663
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-20 17:14:30 +0900

    コードのリファクタ

--- MinuteRepeaterTest/ViewController.swift
+++ MinuteRepeaterTest/ViewController.swift
@@ -13,69 +13,60 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
-
+  
   @IBAction func actionButton(_ sender: UIButton) {
     
     let now = Date()
     let time = Calendar.current.dateComponents([.hour, .minute], from: now)
     
-    var hour = time.hour!
-
-    if hour > 12 {
-      hour -= 12
-    }
-    
+    // hour を12時間で丸める
+    let hour = time.hour! % 12
     let quarter = time.minute! / 15
     let minute = time.minute! % 15
-    
-    var hourCount = 0
-    var quarterCount = 0
-    var minuteCount = 0
-    
-    let lowGong:SystemSoundID = 1013
-    //    候補1008, 1054
-    let highGong:SystemSoundID = 1054
 
+    // 低音ゴング
+    let lowGong:SystemSoundID = 1054
+    // 高音ゴング
+    let highGong:SystemSoundID = 1013
+    
     // 第一ゴング
-    while hourCount < hour {
-      print(hourCount + 1, "dong")
-      
-      hourCount += 1
-      
+    for _ in 0 ..< hour {
+      print("dong")
       AudioServicesPlaySystemSound(lowGong)
       
-      // 0.7秒待つ
+      // 0.5秒待つ
+      Thread.sleep(forTimeInterval: 0.5)
+    }
+    
+    // 第二ゴングが鳴る場合遅延処理
+    if quarter > 1 {
       Thread.sleep(forTimeInterval: 0.7)
     }
     
     // 第二ゴング
-    if quarter > 0 {
-      while quarterCount < quarter {
-        print(quarterCount + 1, "ding-dong")
-        quarterCount += 1
-        
-        AudioServicesPlaySystemSound(highGong)
-        
-        // 0.7秒待つ
-        Thread.sleep(forTimeInterval: 0.5)
-        
-        AudioServicesPlaySystemSound(lowGong)
-        
-        // 0.7秒待つ
-        Thread.sleep(forTimeInterval: 0.7)
-      }
+    for _ in 0 ..< quarter {
+      print("ding-dong")
+      AudioServicesPlaySystemSound(highGong)
+      // 0.4秒待つ
+      Thread.sleep(forTimeInterval: 0.4)
+      AudioServicesPlaySystemSound(lowGong)
+      
+      // 0.5秒待つ
+      Thread.sleep(forTimeInterval: 0.5)
+    }
+    
+    // 第三ゴングが鳴る場合遅延処理
+    if minute > 1 {
+      Thread.sleep(forTimeInterval: 0.7)
     }
     
     // 第三ゴング
-    if minute > 0 {
-      while minuteCount < minute {
-        print(minuteCount + 1, "ding")
-        minuteCount += 1
-        
-        AudioServicesPlaySystemSound(highGong)
-        // 0.7秒待つ
-        Thread.sleep(forTimeInterval: 0.7)
-      }
+    for _ in 0 ..< minute {
+      print("ding")
+      AudioServicesPlaySystemSound(highGong)
+
+      // 0.5秒待つ
+      Thread.sleep(forTimeInterval: 0.5)
     }
   }
 }
