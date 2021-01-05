bdb3d65652de0425b70a8ffa5513868a3b6bd8b7
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-05 20:29:14 +0900

    重複しない処理の実装

--- MinuteRepeaterTest/ViewController.swift
+++ MinuteRepeaterTest/ViewController.swift
@@ -10,92 +10,80 @@ import AudioToolbox
 
 class ViewController: UIViewController {
   
+  private var isPlaying = false
+  
   @IBOutlet weak var button: UIButton!
-    
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
   @IBAction func actionButton(_ sender: UIButton) {
+    if isPlaying { return }
+    isPlaying = true
     
-    self.button.isEnabled = false
-    
-    DispatchQueue.main.async {
-      self.repeater()
-
-    }
-    
-    
-
-//    if button.isHidden == true {
-//      print(button.isHidden)
-//    }
-//    repeater()
-//    button.isHidden = false
+    repeater()
   }
+  
   
   func repeater() {
     
-//    button.isHidden = true
-
-    let now = Date()
-    let time = Calendar.current.dateComponents([.hour, .minute], from: now)
-    
-    // hour を12時間で丸める
-    let hour = time.hour! % 12
-    let quarter = time.minute! / 15
-    let minute = time.minute! % 15
-
-    // 低音ゴング
-    let lowGong:SystemSoundID = 1054
-    // 高音ゴング
-    let highGong:SystemSoundID = 1013
-    
-    // 第一ゴング
-    for _ in 0 ..< hour {
-      print("dong")
-      AudioServicesPlaySystemSound(lowGong)
+    DispatchQueue.global().async {
       
-      // 0.5秒待つ
-      Thread.sleep(forTimeInterval: 0.5)
-    }
-    
-    // 第二ゴングが鳴る場合遅延処理
-    if quarter > 0 {
-      Thread.sleep(forTimeInterval: 0.7)
-    }
-    
-    // 第二ゴング
-    for _ in 0 ..< quarter {
-      print("ding-dong")
-      AudioServicesPlaySystemSound(highGong)
-      // 0.4秒待つ
-      Thread.sleep(forTimeInterval: 0.4)
-      AudioServicesPlaySystemSound(lowGong)
+      let now = Date()
+      let time = Calendar.current.dateComponents([.hour, .minute], from: now)
       
-      // 0.5秒待つ
-      Thread.sleep(forTimeInterval: 0.5)
-    }
-
-    // 第三ゴングが鳴る場合遅延処理
-    if minute > 0 {
-      Thread.sleep(forTimeInterval: 0.7)
-    }
-
-    // 第三ゴング
-    for index in 0 ..< minute {
-      print("ding")
-      AudioServicesPlaySystemSound(highGong)
+      // hour を12時間で丸める
+      let hour = time.hour! % 12
+      let quarter = time.minute! / 15
+      let minute = time.minute! % 15
       
-      // 0.5秒待つ
-      Thread.sleep(forTimeInterval: 0.5)
+      // 低音ゴング
+      let lowGong:SystemSoundID = 1054
+      // 高音ゴング
+      let highGong:SystemSoundID = 1013
       
-//      if index == minute - 1 {
-//        print("(´・ω・｀)")
-//        self.button.isEnabled = true
-//      }
+      // 第一ゴング
+      for _ in 0 ..< hour {
+        print("dong")
+        AudioServicesPlaySystemSound(lowGong)
+        
+        // 0.5秒待つ
+        Thread.sleep(forTimeInterval: 0.5)
+      }
+      
+      // 第二ゴングが鳴る場合遅延処理
+      if quarter > 0 {
+        Thread.sleep(forTimeInterval: 0.7)
+      }
+      
+      // 第二ゴング
+      for _ in 0 ..< quarter {
+        print("ding-dong")
+        AudioServicesPlaySystemSound(highGong)
+        // 0.4秒待つ
+        Thread.sleep(forTimeInterval: 0.4)
+        AudioServicesPlaySystemSound(lowGong)
+        
+        // 0.5秒待つ
+        Thread.sleep(forTimeInterval: 0.5)
+      }
+      
+      // 第三ゴングが鳴る場合遅延処理
+      if minute > 0 {
+        Thread.sleep(forTimeInterval: 0.7)
+      }
+      
+      // 第三ゴング
+      for _ in 0 ..< minute {
+        print("ding")
+        AudioServicesPlaySystemSound(highGong)
+        
+        // 0.5秒待つ
+        Thread.sleep(forTimeInterval: 0.5)
+      }
+      
+      self.isPlaying = false
     }
-    
-
   }
 }
